d2f030d9528d185b82210ee0ec15efd5c832d0d0
aalaebl <aalaeedine2000@outlook.fr>
2023-04-02 19:56:16 +0000

    Fix the bypass

--- src/services/jobs/jobs.services.ts
+++ src/services/jobs/jobs.services.ts
@@ -65,14 +65,21 @@ export async function getJobList(
 
   const title = await page.title();
 
+  if (title == "Before you continue to Google Search") {
+    await page.$eval(
+      "#yDmH0d > c-wiz > div > div > div > div.NIoIEf > div.G4njw > div.AIC7ge > div.CxJub > div.VtwTSb > form:nth-child(2) > div > div > button",
+      (button) => button.click()
+    );
+
+    await page.waitForNavigation({ waitUntil: "networkidle2" });
+  }
+
   const jobList = await scrapeInfiniteScrollItems(
     page,
     extractJobsList,
     GOOGLE_SELECTORS.scrollableItem,
     resultsCount
   );
-
-  console.log(page);
 
   await browser.close();
   return [jobList, url, title, page];
--- src/utils/scrapeInfiniteScrollItems.ts
+++ src/utils/scrapeInfiniteScrollItems.ts
@@ -14,10 +14,6 @@ export async function scrapeInfiniteScrollItems<T>(
     let numberOfTrials = 0;
 
     while (items.length < itemCount && numberOfTrials < 2) {
-      if ((await page.title()).includes("Before you continue")) {
-        page.$eval("#L2AGLb > div", (button) => button.click());
-      }
-
       items = await page.evaluate(extractItems);
 
       previousHeight = await page.evaluate(`${scrollElement}.scrollHeight`);
