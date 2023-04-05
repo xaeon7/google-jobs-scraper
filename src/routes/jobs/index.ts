9338491dbf66060ac6b8057d89ad28b8a9fc693e
aalaebl <aalaeedine2000@outlook.fr>
2023-04-05 21:54:50 +0000

    Added Cache

--- src/routes/jobs/index.ts
+++ src/routes/jobs/index.ts
@@ -5,12 +5,14 @@ import {
 } from "../../controllers/jobs/jobs.controller";
 import validateResource from "../../middleware/validateResource";
 import { jobsListRequestSchema } from "../../schema/jobs/jobs.schema";
+import { cache } from "../../middleware/cache";
 
 const router = express.Router();
 
 //? GET
 router.get(
   "/list",
+  cache,
   validateResource(jobsListRequestSchema),
   getJobsListHandler
 );
